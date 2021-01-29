4984918583c92d8f7cdbe7a4408532e2b36b2b5e
Magic-Kowel <58827128+Magic-Kowel@users.noreply.github.com>
2021-01-28 21:40:19 -0600

    form de la pafina

---
+++ frontend/app/src/pages/FormNew.js
@@ -0,0 +1,12 @@
+import React from 'react';
+import FormRegister from '../components/FormRegister';
+class FormNew extends React.Component {
+    render() {
+        return (
+            <div className="container">
+                <FormRegister />
+            </div>
+        );
+    }
+}
+export default  FormNew;
